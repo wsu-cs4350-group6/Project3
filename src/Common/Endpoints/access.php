36d2b2f19353cdc376050587ea891cb800b62e36
Darin Critchlow <darincritchlow@gmail.com>
2015-04-05 22:25:01 -0600

    Add apidocs and corresponding responses

--- src/Common/Endpoints/access.php
+++ src/Common/Endpoints/access.php
@@ -2,6 +2,22 @@
 
 use API\Common\Authentication\SQLiteAccess;
 
+/**
+ * @api {get} /access Access Key Request
+ * @apiName GetAccess
+ * @apiGroup Authenticate
+ *
+ * @apiSuccess {String} key Access Key
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *        "key": "e618d316-8249-5d7a-8eac-8942f73192d7"
+ *     }
+ *
+ * @apiVersion 0.1.0
+ */
+
 $app->get('/access', function() use ($app, $env) {
 
     $access = new SQLiteAccess();
